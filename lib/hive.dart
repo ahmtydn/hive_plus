83eeb962028803fca6672b1f0eab23aa02916cca
ahmtydn <ahmttyydn@gmail.com>
2025-09-23 02:24:46 +0300

    init

---
+++ lib/hive.dart
@@ -0,0 +1,10 @@
+/// Hive Plus is a lightweight and blazing fast key-value store made for\n/// Flutter and Dart. It is strongly encrypted using AES-256.
+library hive_plus;
+
+import 'package:hive_plus/src/impl/frame.dart';
+import 'package:isar_plus/isar_plus.dart';
+
+part 'src/box.dart';
+part 'src/hive.dart';
+part 'src/impl/box_impl.dart';
+part 'src/impl/type_registry.dart';
